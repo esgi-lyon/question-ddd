68d1e5af55cdb82da46480fa25cd359c8a8a8fed
loic-roux-404 <loic.roux.404@gmail.com>
2023-04-21 22:51:13 +0200

    Initial version of UserManagementContext generated by generator-jhipster@7.9.3

---
+++ apps/UserManagementContext/src/main/java/org/contextmapper/generated/usermanagementcontext/config/Constants.java
@@ -0,0 +1,11 @@
+package org.contextmapper.generated.usermanagementcontext.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+
+    private Constants() {}
+}
